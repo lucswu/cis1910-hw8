e69dcdfbff62951361133c904534a81f997ca437
Lucas Wu <lucaswu@wharton.upenn.edu>
2023-04-03 22:13:39 -0400

    done

--- commands.sh
+++ commands.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# Lucas Wu, HW8
+
 function problem1 {
   git log -n 1
 }
 
 function problem2 {
-  
+  git diff HEAD~2  
 }
 
 function problem3 {
-
+  git annotate menus/menus.txt  
 }
